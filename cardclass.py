9d2d2c0f8dbe6568991505289052c545488f0d5d
James Lao <Jlprime@users.noreply.github.com>
2022-01-07 12:12:44 +0800

    weather

--- cardclass.py
+++ cardclass.py
@@ -1,7 +1,22 @@
+import requests
+import json
 
+def cardClass(lat, long):
+    def __init__(self):
+        self.weather = weather(lat, long)
+        self.eatPlace = eatPlace(lat, long)
+        self.visitPlace = visitPlace(lat, long)
 
-def cardClass(weather, eatPlace, visitPlace):
-    def __init__ (self, weather, eatPlace, visitPlace):
-        self.weather = weather
-        self.eatPlace = eatPlace
-        self.visitPlace = visitPlace
+def weather(lat, long):
+    api_key = "5649fe25da87c63923302dbd96adf26e"
+    lat = lat
+    lon = long
+    url = "api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (lat, lon, api_key)
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+    print(data)
+    #current = data["çurrent"]["weather"]["main"]
+    #return current
+
+#weather(1.3521,103.8198)
--- geolocation.py
+++ geolocation.py
@@ -10,5 +10,7 @@ geolocator = Nominatim(user_agent="@hnrwanderbot")
 def location (message):
     lat = message.location.latitude
     long = message.location.longitude
-    cityLocation = geolocator.reverse('{} {}'.format(lat, long))
-    return cityLocation.address
+    return lat, long
+
+def deriveCity(lat, long):
+    return geolocator.reverse('{} {}'.format(lat, long)).address
