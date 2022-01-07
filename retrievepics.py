aba930be2ac722c5ebafb03d480c76526433ca0d
James Lao <Jlprime@users.noreply.github.com>
2022-01-08 06:12:24 +0800

    Update retrievepics.py

--- retrievepics.py
+++ retrievepics.py
@@ -3,9 +3,3 @@ import config
 def retrievePics (reference):
     url = "https://maps.googleapis.com/maps/api/place/photo?maxheight=250&photo_reference=%s&key=%s" % (reference, config.GG_API_KEY)
     return url
-
-    # print(url)
-    # response = requests.get(url)
-    # file = open("sample_image.png","wb")
-    # file.write(response.content)
-    # file.close()
